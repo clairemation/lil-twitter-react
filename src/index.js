ac0abbb6a1a113eddc42719ff644170f3f284b9c
Claire Samuels <clairemation@gmail.com>
2017-05-17 11:50:50 -0400

    Extract tweet to tweet.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TweetStream from './tweet-stream';
+import './index.css';
+
+ReactDOM.render(
+  <TweetStream />,
+  document.getElementById('tweet-stream')
+);
---
+++ src/tweet-stream.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Tweet from './tweet'
+
+class TweetStream extends Component {
+
+  render(){
+    return (
+      <Tweet />
+    )
+  }
+}
+
+export default TweetStream
