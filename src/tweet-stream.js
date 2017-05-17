7d51e8a0eced4c60a103baada4e700cbb33d8169
Claire Samuels <clairemation@gmail.com>
2017-05-17 12:01:08 -0400

    Add stubbed data

--- src/tweet-stream.js
+++ src/tweet-stream.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 import Tweet from './tweet'
+import tweetStubData from './tweets-stub-data'
+
+const STUB_MODE = true
 
 class TweetStream extends Component {
+  constructor(args) {
+    super(args)
+    this.state = {data: []}
+  }
+
+  componentWillMount() {
+    if (STUB_MODE) this.setState({data: tweetStubData})
+  }
 
   render(){
     return (
-      <Tweet />
+      <Tweet data={this.state.data[0]} />
     )
   }
 }
--- src/tweet.js
+++ src/tweet.js
@@ -5,14 +5,14 @@ class TweetStream extends Component {
   render(){
     return (
       <li className="tweet">
-        <img className="avatar" src="https://pbs.twimg.com/profile_images/2858734172/db7da718fd369eba6184973631d7d84a_bigger.jpeg" alt="" />
+        <img className="avatar" src={this.props.data.avatar_url} alt="" />
         <div className="tweet-content">
           <p>
-            <span className="full-name">Sam Samskies</span>
-            <span className="username">@SamSamskies</span>
+            <span className="full-name">{this.props.data.username}</span>
+            <span className="username">{this.props.data.handle}</span>
             <span className="timestamp">- 6m</span>
           </p>
-          <p>My latest contribution to the advancement of the human race. I present to you ver 0.0.1 of http://mileyroids.com/  #Mileyroids #miley haha :)</p>
+          <p>{this.props.data.content}</p>
         </div>
       </li>
     )
